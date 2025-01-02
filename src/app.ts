fd2e8fce555e214dfe3e0be8b053275975303af2
mitesh4000 <somone_from_hell@tutanota.com>
2025-01-02 14:30:49 +0530

    added som folders to gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,18 @@
 import dotenv from "dotenv";
 import express, { Application } from "express";
+import { createServer } from "http";
 import morgan from "morgan";
+import path from "path";
+import { Server } from "socket.io";
 import { serverStatus } from "./controller/view.controller";
 import authRoutes from "./routes/auth.routes";
 import contractRoutes from "./routes/contract.routes";
 import milestoneRoutes from "./routes/milestone.routes";
 import projectRoutes from "./routes/project.routes";
 import userRoutes from "./routes/user.routes";
-// import swaggerDocs from "./swagger";
-import { createServer } from "http";
-import path from "path";
-import { Server } from "socket.io";
 import { connectToDatabase } from "./utils/connectToDb";
 import { validateEnv } from "./utils/validateEnv";
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output.json");
 dotenv.config();
 
 const app: Application = express();
@@ -27,7 +24,6 @@ connectToDatabase();
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.static(path.resolve("./src/public")));
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/", serverStatus);
 app.use("/auth", authRoutes);
@@ -35,12 +31,11 @@ app.use("/user", userRoutes);
 app.use("/contract", contractRoutes);
 app.use("/", milestoneRoutes);
 app.use("/", projectRoutes);
-// swaggerDocs(app, Number(port));
 
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port} in ${process.env.NODE_ENV} mode`);
 });
